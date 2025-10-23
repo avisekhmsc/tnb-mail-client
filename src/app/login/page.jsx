9b32f791847922e3cc036b3eaaf9e5bbec2da3a5
avisekhmsc <avisekh.microace@gmail.com>
2025-10-23 11:14:41 +0530

    Added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -72,13 +72,13 @@ const Login = () => {
     }
   };
 
-  if (authToken && user) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-gray-100">
-        <Loader />
-      </div>
-    );
-  }
+  // if (authToken && user) {
+  //   return (
+  //     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-gray-100">
+  //       <Loader />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
